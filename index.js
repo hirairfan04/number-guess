a9400decd5d2dd0ba1941e7204b98c838af2a1e0
Hira Irfan <162793914+hirairfan04@users.noreply.github.com>
2024-03-23 00:34:11 +0500

    number guess code files

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessednumber",
+        type: "number",
+        message: "Please guess a number between 1-6",
+    }
+]);
+if (answer.userGuessednumber === randomNumber) {
+    console.log("Congratulations! you won");
+}
+else {
+    console.log("You guessed wrong number");
+}
